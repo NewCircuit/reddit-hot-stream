80f2e05f7b60cb6eb02b47dcd7b1b94a26be5b5a
Matthew <staalkoper@gmail.com>
2020-08-13 23:01:02 +0200

    fixed webhook bug and rate limit handling

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 import time
 from configparser import ConfigParser
 
-
 conf = ConfigParser()
 conf.read('conf.ini')
 
@@ -13,8 +12,6 @@ reddit = praw.Reddit(client_id=conf.get('reddit', 'client_id'),
                      password=conf.get('reddit', 'password'),
                      user_agent=conf.get('reddit', 'user_agent'))
 
-webhook = DiscordWebhook(url=conf.get('discord', 'webhook_url'))
-
 posts = {}
 
 
@@ -22,15 +19,17 @@ def main():
     for submission in reddit.subreddit("pewdiepiesubmissions").hot():
         if not submission.over_18 and (submission.score >= 20000):
             if submission.id not in posts:
-                print(submission.score)
+                webhook = DiscordWebhook(url=conf.get('discord', 'webhook_url'))
                 embed = DiscordEmbed(title=submission.title, color=0xf9013f)
-                embed.set_author(name=submission.author.name, url='https://reddit.com' + submission.permalink, icon_url=submission.author.icon_img)
+                embed.set_author(name=submission.author.name, url='https://reddit.com' + submission.permalink,
+                                 icon_url=submission.author.icon_img)
                 embed.set_image(url=submission.url)
                 embed.set_footer(text=f'👍 {submission.score} | 💬 {submission.num_comments}')
 
                 webhook.add_embed(embed)
                 webhook.execute()
                 posts[submission.id] = time.time()
+                time.sleep(5)  # To prevent rate limit
 
 
 if __name__ == '__main__':
@@ -42,4 +41,4 @@ if __name__ == '__main__':
             if timestamp < day_from_now:  # Check if submission is one day old
                 posts.pop(submission_id)
 
-        time.sleep(60*60)  # Run every 10 minutes
+        time.sleep(60 * 60)  # Run every 10 minutes
